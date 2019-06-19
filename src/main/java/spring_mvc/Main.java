3839d5fbf0e8ebd6466b48574a9805550e6c3800
satonin18 <satonin18@mail.ru>
2019-06-19 18:12:24 +0500

    added class and html and controller

---
+++ src/main/java/spring_mvc/Main.java
@@ -0,0 +1,13 @@
+package spring_mvc;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
