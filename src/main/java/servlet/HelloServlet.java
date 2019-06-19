1a63f0bbef399294fc59f2e708040f208d798d1b
satonin18 <satonin18@mail.ru>
2019-06-19 17:28:23 +0500

    added files

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Welcome to servlets</h2>");
+        } finally {
+            writer.close();
+        }
+    }
+}
