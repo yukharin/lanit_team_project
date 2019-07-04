431ef711fcb895a5b128517dd6e03b33f89338f5
vlad_yukharin <yukharinvlad@gmail.com>
2019-07-04 17:15:56 +0500

    Added almost nothing

--- src/main/java/com/lanit/lkz_project/controllers/default_controllers/PersonalAccountController.java
+++ src/main/java/com/lanit/lkz_project/controllers/default_controllers/PersonalAccountController.java
@@ -2,8 +2,10 @@ package com.lanit.lkz_project.controllers.default_controllers;
 
 import com.lanit.lkz_project.entities.Action;
 import com.lanit.lkz_project.entities.Notification;
+import com.lanit.lkz_project.entities.Organization;
 import com.lanit.lkz_project.service.ActionService;
 import com.lanit.lkz_project.service.NotificationService;
+import com.lanit.lkz_project.service.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
@@ -18,9 +23,11 @@ import java.util.List;
 public class PersonalAccountController {
 
     @Autowired
-    ActionService actionService;
+    private ActionService actionService;
     @Autowired
     private NotificationService notificationService;
+    @Autowired
+    private OrganizationService organizationService;
 
     @GetMapping("/")
     public String toPersonalAccount() {
@@ -49,5 +56,26 @@ public class PersonalAccountController {
         return "redirect:/account";
     }
 
+    @GetMapping("/account/getAddingPage")
+    public String getAddNotificationPage(Model model) {
+        List<Organization> organizations = organizationService.organizations();
+        model.addAttribute("organizations", organizations);
+        return "addNotification";
+    }
+
+
+    @PostMapping("/account/addNotification")
+    public String addNotification(HttpServletRequest request) throws ParseException {
+        String notificationType = request.getParameter("notificationType");
+        Date dateResponse = new SimpleDateFormat("dd-mm-yyyy").parse(request.getParameter("dateResponse"));
+        Long orgId = Long.valueOf(request.getParameter("orgId"));
+        Notification notification = new Notification();
+        notification.setNotificationType(notificationType);
+        notification.setDateResponse(dateResponse);
+        notification.setDateRecieved(new Date());
+        notification.setOrganization(organizationService.getOrganization(orgId));
+        notificationService.addNotification(notification);
+        return "redirect:/account";
+    }
 
 }
