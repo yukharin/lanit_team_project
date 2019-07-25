a090623280f29efa081ef5c0cd4db280c363d38e
vlad_yukharin <yukharinvlad@gmail.com>
2019-07-25 19:13:52 +0500

    Now user registration with JSON

--- src/main/resources/static/js/personalAcc.js
+++ src/main/resources/static/js/personalAcc.js
@@ -2,7 +2,7 @@ var cleanFiltersButton = document.getElementById("clean_filters");
 var applyFiltersButton = document.getElementById("apply_filters");
 
 function myFunction() {
-    
+
 }
 cleanFiltersButton.addEventListener("click", myFunction);
 applyFiltersButton.addEventListener("click", myFunction);
