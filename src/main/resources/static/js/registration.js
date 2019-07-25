2c25616ea966c9ba851ebc472e625a1db56b1667
vlad_yukharin <yukharinvlad@gmail.com>
2019-07-25 18:24:16 +0500

    Now user registration with JSON

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -1,7 +1,16 @@
 var regButton = document.getElementById("submitRegistration");
+
+
 var registrationFunction = function () {
+    var json = generateJsonFromForm();
+    sendUserData(json);
+};
 
+if (regButton) {
+    regButton.addEventListener("click", registrationFunction);
+}
 
+function generateJsonFromForm() {
     var form = document.getElementById("register-form");
     var formData = new FormData(form);
     var object = {
@@ -16,24 +25,18 @@ var registrationFunction = function () {
             object[key] = value;
         }
     });
-    var json = JSON.stringify(object);
-    console.log(json);
-    var xhr = new XMLHttpRequest();
-    var url = "http://localhost:8080/lkz_project_war/registerUser/";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var som = JSON.parse(xhr.responseText);
-            console.log(som);
-        }
-    };
-    xhr.send(json);
-};
-
-if (regButton) {
-    regButton.addEventListener("click", registrationFunction);
+    var result = JSON.stringify(object);
+    return result;
 }
+
+function sendUserData(json) {
+    var request = new XMLHttpRequest();
+    var url = "http://localhost:8080/lkz_project_war/registerUser/";
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(json);
+}
+
 
 
 
