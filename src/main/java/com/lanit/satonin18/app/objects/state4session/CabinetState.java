9a7c0454469b7515a7e4789495ee1dba023a1971
satonin18 <satonin18@mail.ru>
2019-08-21 17:59:19 +0500

    serial object in DTO

--- src/main/java/com/lanit/satonin18/app/objects/state4session/CabinetState.java
+++ src/main/java/com/lanit/satonin18/app/objects/state4session/CabinetState.java
@@ -5,12 +5,16 @@ import com.lanit.satonin18.app.objects.input.form.OrderByForm;
 import com.lanit.satonin18.app.objects.input.form.PaginationForm;
 import lombok.Data;
 
+import java.io.Serializable;
+
 //TODO (как сделать так чтобы были свои обьекты(а не дто, у которых пришлось сетеры еще поставить),
 // и чтоб переменные(дто и своих обьектов) были только в одном месте(без дублирование кода при будущих изменениях),
 // и чтоб другому разрабу мое будущие решение было сразу же понятно, а не спустя 10 минут)
 // -> наверн адаптер поставть (или перименовть dto, чтоб не вводить в заблудение)
 @Data
-public class CabinetState {
+public class CabinetState
+        implements Serializable {
+
     FilterForm filterForm;
     PaginationForm paginationForm;
     OrderByForm orderByForm;
--- src/main/java/com/lanit/satonin18/app/objects/state4session/TheNotificationState.java
+++ src/main/java/com/lanit/satonin18/app/objects/state4session/TheNotificationState.java
@@ -4,11 +4,15 @@ import com.lanit.satonin18.app.objects.input.form.OrderByForm;
 import com.lanit.satonin18.app.objects.input.form.PaginationForm;
 import lombok.Data;
 
+import java.io.Serializable;
+
 //TODO (как сделать так чтобы были свои обьекты(а не дто, у которых пришлось сетеры еще поставить),
 // и чтоб переменные были только в одном месте(без дублирование кода при будущих изменениях),
 // и чтоб другому разрабу мое будущие решение было сразу же понятно, а не спустя 10 минут)
 @Data
-public class TheNotificationState {
+public class TheNotificationState
+        implements Serializable {
+
     PaginationForm paginationForm;
     OrderByForm orderByForm;
 }
