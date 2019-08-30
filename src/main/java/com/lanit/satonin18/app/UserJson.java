2e1898ce3f33af38f881386daa1715f4327a5345
satonin18 <satonin18@mail.ru>
2019-08-30 13:03:53 +0500

    v1-передача JSON
    ver1 (Angular and Java)

--- src/main/java/com/lanit/satonin18/app/UserJson.java
+++ src/main/java/com/lanit/satonin18/app/UserJson.java
@@ -1,5 +1,12 @@
 package com.lanit.satonin18.app;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data //get and set
+@AllArgsConstructor
 public class UserJson {
     String name;
     int age;
--- src/main/java/com/lanit/satonin18/app/controller/rest/Angular.java
+++ src/main/java/com/lanit/satonin18/app/controller/rest/Angular.java
@@ -1,20 +1,21 @@
-package com.lanit.satonin18.app.controller;
+package com.lanit.satonin18.app.controller.rest;
 
 import com.lanit.satonin18.app.UserJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@CrossOrigin
+@RestController
 @Controller("angularController")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/angular")
 public class Angular {
 
-    @ResponseBody
-    @RequestMapping("/")
-    public String getPage(UserJson userJson) {
-        
+    @RequestMapping("/user")
+    public /*@ResponseBody*/ UserJson user() {
+        UserJson userJson = new UserJson("ht", 3);
+        return userJson;
     }
 }
