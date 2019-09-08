32d972e819883c114ac78b57caf1f18352f4383e
vlad_yukharin <yukharinvlad@gmail.com>
2019-09-08 20:06:04 +0500

    Now application has 2 modules: Spring Backend and Angular Frontend.

--- Backend/src/main/java/com/lanit/lkz_project/controllers/advice_controllers/ExceptionAdviceController.java
+++ Backend/src/main/java/com/lanit/lkz_project/controllers/advice_controllers/ExceptionAdviceController.java
@@ -2,7 +2,11 @@ package com.lanit.lkz_project.controllers.advice_controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @ControllerAdvice
@@ -16,27 +20,27 @@ public class ExceptionAdviceController {
     @Value("${error_403_page}")
     private String error_403_page;
 
-//    @ExceptionHandler(Throwable.class)
-//    public ModelAndView handle(Throwable ex) {
-//        log.error("Exception: ", ex);
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName(general_error_page);
-//        modelAndView.addObject("reason", ex.getMessage());
-//        return modelAndView;
-//    }
-//
-//
-//    @ExceptionHandler(NoHandlerFoundException.class)
-//    public String handleError404(Exception e) {
-//        log.error("404 error: ", e);
-//        return error_404_page;
-//    }
-//
-//
-//    @ExceptionHandler(AccessDeniedException.class)
-//    public String handleError403(Exception e) {
-//        log.error("Access denied: ", e);
-//        return error_403_page;
-//    }
+    @ExceptionHandler(Throwable.class)
+    public ModelAndView handle(Throwable ex) {
+        log.error("Exception: ", ex);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName(general_error_page);
+        modelAndView.addObject("reason", ex.getMessage());
+        return modelAndView;
+    }
+
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleError404(Exception e) {
+        log.error("404 error: ", e);
+        return error_404_page;
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleError403(Exception e) {
+        log.error("Access denied: ", e);
+        return error_403_page;
+    }
 
 }
